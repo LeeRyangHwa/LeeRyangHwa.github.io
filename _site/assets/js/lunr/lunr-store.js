4b8fb86345681619536ab67c6606419696a46225
LeeRyangHwa <iryanghwa@gmail.com>
2020-11-10 22:38:47 +0900

    블로그 설정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":"GitHub Blog 서비스 테스트  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "http://localhost:4000/blog/first-post/",
+        "teaser": null
+      }]
