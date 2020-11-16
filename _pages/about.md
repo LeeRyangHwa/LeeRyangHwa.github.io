a67f6db23e5aa40b68bd57b926025fb13f6d5aa3
LeeRyangHwa <iryanghwa@gmail.com>
2020-11-16 15:17:53 +0900

    add about

---
+++ _pages/about.md
@@ -0,0 +1,9 @@
+---
+title: "이 블로그 설명서"
+permalink: /about/
+layout: single
+---
+
+## github.io 블로그
+
+이 블로그는 GitHub Pages 블로그 서비스인 github.io 블로그 입니다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "github.io 블로그 시작하기",
-        "excerpt":"GitHub Blog 서비스 테스트  ","categories": ["Blog"],
-        "tags": ["Blog"],
-        "url": "http://localhost:4000/blog/first-post/",
+        "excerpt":"GitHub Blog 서비스 테스트 GitHub Blog 시작  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/first-post/",
         "teaser": "http://localhost:4000/assets/images/logo.png"
       }]
